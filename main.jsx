34f50bc4bc4ee3dce0f881b49f5f503c32152154
Cahyo <nurcahya.priantoro@apps.ipb.ac.id>
2025-11-21 18:09:18 +0700

    fix: use HashRouter to avoid server rewrite for SPA routes

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import BaksoFinanceApp from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
+	<HashRouter>
 		<BaksoFinanceApp />
-	</BrowserRouter>
+	</HashRouter>
 );
